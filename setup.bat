aa6ea7a49850a258be56d66169c9c818caae02ae
Adam Sitabkhan <adam.sitabkhan@gmail.com>
2025-12-28 18:35:12 -0600

    Added ffmpeg install command to setup script

--- setup.bat
+++ setup.bat
@@ -4,13 +4,17 @@ echo ===========================================
 echo   YouTube to MP3 CLI - Setup (Windows)
 echo ===========================================
 
-:: 1. Install Python Libraries
-echo [1/2] Installing required Python libraries...
+:: 1. Install ffmpeg
+echo [1/3] Installing ffmpeg...
+winget install ffmpeg
+
+:: 2. Install Python Libraries
+echo [2/3] Installing required Python libraries...
 python -m pip install --upgrade pip
 python -m pip install yt-dlp mutagen python-dotenv prompt_toolkit
 
-:: 2. Initialize .env file
-echo [2/2] Checking for .env file...
+:: 3. Initialize .env file
+echo [3/3] Checking for .env file...
 if not exist .env (
     echo OUTPUT_FOLDER='' > .env
     echo ✅ Created new .env file with OUTPUT_FOLDER=''
